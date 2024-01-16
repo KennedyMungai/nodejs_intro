74c7d32275676fe0cd786896909d6209b3ca86eb
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 04:26:13 +0300

    Added the directory name for the path

--- server.js
+++ server.js
@@ -2,7 +2,10 @@
 // console.log(global)
 
 const os = require('os')
+const path = require('path')
 
 // console.log(os.platform(), os.homedir(), os.hostname(), os.type(), os.version())
-console.log(__dirname)
-console.log(__filename)
+// console.log(__dirname)
+// console.log(__filename)
+
+console.log(path.dirname(__filename))
