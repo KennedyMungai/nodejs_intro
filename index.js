37218eb1ebdbb1d6163fc3c442e7cca7922b3db9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 10:17:02 +0300

    Added a writeFile function call to the index js

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8',(err, data) =>
 
 console.log('Hello...')
 
+fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'This is some text to be written to the file' ,(err) => {
+    if(err) throw err;
+    console.log('File written successfully');
+})
+
 // exit on uncaught errors
 process.on('uncaughtException', err => {
     console.error(`There was an uncaught error: ${err}`)
