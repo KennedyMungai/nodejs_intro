3c830d68321d0122d8d923807301f07a204f9706
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 10:29:08 +0300

    Commented out some more code

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 const path = require('path')
 
 
-fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8',(err, data) => {
-    if(err) throw err;
-    console.log(data);
-})
+// fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8',(err, data) => {
+//     if(err) throw err;
+//     console.log(data);
+// })
 
-console.log('Hello...')
+// console.log('Hello...')
 
 // fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'This is some text to be written to the file' ,(err) => {
 //     if(err) throw err;
