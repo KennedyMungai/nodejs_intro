47a45e664f2d4ac1f0835a211e7af7e0ad3e258f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 10:07:37 +0300

    Using UTF-8 encoding to read the data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 
-fs.readFile('./files/starter.txt', (err, data) => {
+fs.readFile('./files/starter.txt', 'utf-8',(err, data) => {
     if(err) throw err;
-    console.log(data.toString());
+    console.log(data);
 })
