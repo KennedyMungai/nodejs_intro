731c36fc200d7be734bf7a4df55221a014a00354
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 10:13:10 +0300

    Updated the call for the starter txt file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 
 
-fs.readFile(path.join(__dirname, 'files/starter.txt'), 'utf-8',(err, data) => {
+fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8',(err, data) => {
     if(err) throw err;
     console.log(data);
 })
