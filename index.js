a06c8b7c74e58d29552341815883a445aa1b8288
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 12:12:23 +0300

    Updated the uuid version being used

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const moment = require('moment')
-const { v5: uuid } = require('uuid')
+const { v4: uuid } = require('uuid')
 
 console.log(moment(new Date()).format('YYYY-MM-DD HH:mm:ss'))
 
-console.log(":Henlo")
+console.log(uuid())
