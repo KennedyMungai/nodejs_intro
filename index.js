618c9dadf6e114511f9eca75ac7c81161c66963d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 10:28:22 +0300

    Commented out some of the filesystem callback function calls

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ fs.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf-8',(err, data) =>
 
 console.log('Hello...')
 
-fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'This is some text to be written to the file' ,(err) => {
-    if(err) throw err;
-    console.log('File written successfully');
+// fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), 'This is some text to be written to the file' ,(err) => {
+//     if(err) throw err;
+//     console.log('File written successfully');
     
-    fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), 'Some text to be appended to the file', (err) => {
-        if(err) throw err
-        console.log('File appended successfully')
-    })
+//     fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), 'Some text to be appended to the file', (err) => {
+//         if(err) throw err
+//         console.log('File appended successfully')
+//     })
     
-    fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'renamed_reply.txt'), (err) => {
-        if(err) throw err
-        console.log('File renamed successfully')
-    })
-})
+//     fs.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'renamed_reply.txt'), (err) => {
+//         if(err) throw err
+//         console.log('File renamed successfully')
+//     })
+// })
 
 
 // exit on uncaught errors
