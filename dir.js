262d274796ef61b622abf7ecf061f81efd2b7c4c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2024-01-16 10:52:35 +0300

    Added a checker if statement for if the the directory exists

--- dir.js
+++ dir.js
@@ -1,7 +1,9 @@
 const fs = require('fs')
 
 
-fs.mkdir('./new', (err) => {
-    if(err) throw err
-    console.log('Directory created')
-})
+if (!fs.existsSync('./new')) {
+    fs.mkdir('./new', (err) => {
+        if (err) throw err
+        console.log('Directory created')
+    })
+}
